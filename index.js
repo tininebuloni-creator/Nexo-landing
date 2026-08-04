b4e7cb1577524083540ccbad5efa16f2c86b9e01
tininebuloni-creator <tininebuloni@gmail.com>
2026-08-04 20:39:53 -0300

    Update response message in fetch function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    return new Response("Hola desde mi Worker!", {
+    return new Response("Hola desde Nexo-landing!", {
       headers: { "content-type": "text/plain" },
     });
   },
