a9da5f4f2b9aa4a527f2b42189d492abcaa35f08
tininebuloni-creator <tininebuloni@gmail.com>
2026-08-04 21:07:14 -0300

    Refactor fetch function to return index.html

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 export default {
-  async fetch(request) {
-    return new Response("Hola desde Nexo-landing!", {
-      headers: { "content-type": "text/plain" },
+  async fetch(request, env, ctx) {
+    // Devuelve el contenido de index.html
+    return new Response(await env.ASSETS.fetch("index.html"), {
+      headers: { "content-type": "text/html" },
     });
-  },
+  }
 };
+
