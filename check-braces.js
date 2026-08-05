df97cb08573ed4b110fecf1d00c89ea1b5a6872c
tininebuloni-creator <tininebuloni@gmail.com>
2026-08-05 20:31:01 -0300

    Add files via upload

---
+++ check-braces.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('nexo-agro-erp.html', 'utf8');
+const scriptStart = file.indexOf('<script>');
+const scriptEnd = file.indexOf('</script>');
+
+if (scriptStart >= 0 && scriptEnd > scriptStart) {
+    const scriptContent = file.substring(scriptStart + 8, scriptEnd);
+    
+    let openBraces = 0, closeBraces = 0;
+    let openParens = 0, closeParens = 0;
+    let openBrackets = 0, closeBrackets = 0;
+    
+    for (let i = 0; i < scriptContent.length; i++) {
+        const c = scriptContent[i];
+        if (c === '{') openBraces++;
+        else if (c === '}') closeBraces++;
+        else if (c === '(') openParens++;
+        else if (c === ')') closeParens++;
+        else if (c === '[') openBrackets++;
+        else if (c === ']') closeBrackets++;
+    }
+    
+    console.log(`Llaves: ${openBraces} open, ${closeBraces} close, diff=${openBraces - closeBraces}`);
+    console.log(`Paréntesis: ${openParens} open, ${closeParens} close, diff=${openParens - closeParens}`);
+    console.log(`Brackets: ${openBrackets} open, ${closeBrackets} close, diff=${openBrackets - closeBrackets}`);
+}
