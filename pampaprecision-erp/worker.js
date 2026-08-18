39b9701e25c7a39426aa8320231969a34cd5a1ab
tininebuloni-creator <tininebuloni@gmail.com>
2026-08-18 14:11:24 -0300

    Add files via upload

---
+++ pampaprecision-erp/worker.js
@@ -0,0 +1,11 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/" || url.pathname === "/trial") {
+      url.pathname = "/index.html";
+    }
+
+    return env.ASSETS.fetch(new Request(url, request));
+  }
+};
