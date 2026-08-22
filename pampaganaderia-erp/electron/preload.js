54583d90fa97bb1acc7cb6ecc58926a5f0812af7
tininebuloni-creator <tininebuloni@gmail.com>
2026-08-22 14:47:26 -0300

    Delete pampaganaderia-erp directory

--- pampaganaderia-erp/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAppSettings: () => ipcRenderer.sendSync('get-app-settings'),
-  getServerUrl: () => ipcRenderer.invoke('get-server-url')
-});
